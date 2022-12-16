ffcec69e0f58dea2fb4c314030dfb97ca0f06d67
acacia1021 <acacia1021@gmail.com>
2022-12-16 14:50:46 -0500

    First Assignment Completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,8 @@ import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  //This is an in-line style below, the above line would reference .css file
-  styles: [`h3{
-    color: green;
-  }`]
+  styleUrls: ['./app.component.css']
+ 
 })
 
 
