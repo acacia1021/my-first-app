f8b2851d7195e7b56d90493f165945cc29d89933
acacia1021 <acacia1021@gmail.com>
2022-12-16 14:27:37 -0500

    Templates and Styles Intro app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,11 @@ import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  //This is an in-line style below, the above line would reference .css file
+  styles: [`h3{
+    color: green;
+  }`]
 })
 
 
