ada21931db2e03175a89a735ec4f1520478d155e
acacia1021 <acacia1021@gmail.com>
2022-12-21 15:34:38 -0500

    dynamic property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   // selector: '[app-servers]',
 // selector: '.app-server',
-  // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
   // template: '<app-server></app-server><app-server></app-server>',
-  template: `<app-server></app-server><app-server></app-server>`,
+  // template: `<app-server></app-server><app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+constructor(){
+  //() means function - => does - {} this
+  //, 2000ms = 2 seconds
+  setTimeout(() => {
+    //allowNewServer until 2 seconds reached - allowNewServer defined in html
+    this.allowNewServer = true;
+  }, 2000);
+}
+
+  allowNewServer = false; 
+
 
 }
