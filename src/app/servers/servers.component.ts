c59df3abd69755a405b799b33d5c79bd84f89536
acacia1021 <acacia1021@gmail.com>
2022-12-22 10:01:44 -0500

    Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,27 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   // selector: '[app-servers]',
-// selector: '.app-server',
+  // selector: '.app-server',
   templateUrl: './servers.component.html',
   // template: '<app-server></app-server><app-server></app-server>',
   // template: `<app-server></app-server><app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit{
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!'
+
+  constructor() {
+    //() means function - => does - {} this
+    //, 2000ms = 2 seconds
+    setTimeout(() => {
+      //allowNewServer until 2 seconds reached - allowNewServer defined in html
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
-constructor(){
-  //() means function - => does - {} this
-  //, 2000ms = 2 seconds
-  setTimeout(() => {
-    //allowNewServer until 2 seconds reached - allowNewServer defined in html
-    this.allowNewServer = true;
-  }, 2000);
-}
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
 
-  allowNewServer = false; 
-
-
+  }
 }
