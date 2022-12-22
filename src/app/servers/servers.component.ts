efeb90bba700f9d78e67b3a101f030393fca0984
acacia1021 <acacia1021@gmail.com>
2022-12-22 17:01:50 -0500

    Lists with *ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!'
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer2'];
 
   constructor() {
     //() means function - => does - {} this
@@ -32,6 +33,8 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
     this.serverCreated = true;
+    this.servers.push(this.serverName);
+
   }
 
   // event of type any is passed in
