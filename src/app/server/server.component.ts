d2c204db53b9c879ba14d6175268afd6a71c4679
acacia1021 <acacia1021@gmail.com>
2022-12-22 14:36:47 -0500

    Dynamic ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,18 +1,30 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
+
 //To use the class outside of this file too - like the app.component
-export class ServerComponent{
+export class ServerComponent {
 
     //You dont have to put the type here, typescript can figure it out
     serverId: number = 10;
     serverStatus: string = 'offline';
 
-    getServerStatus(){
+    constructor() {
+        // If true, then online, if false, then offline
+        this.serverStatus = Math.random() > .5 ? 'online' : 'offline'
+    }
+
+
+
+    getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 
 }
